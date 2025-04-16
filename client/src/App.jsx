b33a60053e161a0278593e9d7a336fdde2f8dfd4
Firoj <firojynrr@gmail.com>
2025-04-16 22:08:03 +0530

    Initial MERN setup with client and server

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+export default function App() {
+  return (
+    <>
+      <div className="bg-white p-6 rounded-xl shadow-md w-full max-w-md mx-auto">
+        <h2 className="text-2xl font-semibold text-gray-800 mb-4">
+          Who should win the coding battle?
+        </h2>
+
+        <button className="w-full bg-blue-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition">
+          Option A
+        </button>
+      </div>
+    </>
+  );
+}
